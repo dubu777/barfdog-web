53072b12079e7015089c76a13bd48940533a58f6
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-10-08 18:36:10 +0900

    feat: BottomNavBar 이미지 파일 추가

--- src/components/layout/bottomNavBar/BottomNavBar.css.ts
+++ src/components/layout/bottomNavBar/BottomNavBar.css.ts
@@ -1,5 +1,6 @@
 import { style } from '@vanilla-extract/css';
 
+
 export const bottomNavBarContainer = style({
   position: 'fixed',
   left: 0,
@@ -11,6 +12,6 @@ export const bottomNavBarContainer = style({
   maxWidth: '600px',
   margin: '0 auto',
   height: '66px',
-  backgroundColor: 'black', // 레이아웃 확인용 임시 색상
+  backgroundColor: 'white', // 레이아웃 확인용 임시 색상
   zIndex: 20,
 });
