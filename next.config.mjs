6dc584252c5521873b7a7e3d87bd97f181313ca1
jkeFreshour <jke@freshour.co.kr>
2024-10-10 17:34:36 +0900

    feat: next.config.mjs, package.json, tsconfig.json, axios 기본 설정
    next.config.mjs - 개발/실 서버 배포 환경 구축
    package.json - 프로젝트 scripts 정의
    tsconfig.json -> removeComments - 주석 숨김

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,73 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const dev = process.env.NODE_ENV === 'development';
+const prod = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  crossOrigin: 'anonymous',
+  productionBrowserSourceMaps: true,
+  webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
-    return config;
+    if (prod) config.devtool = 'hidden-source-map';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+    };
   },
+  images: {
+    domains: ['localhost', 'www.barfdogserver.com', 'dev.barfdogserver.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.barfdogserver.com',
+        pathname: '**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    console.log('Default API URL (DEV): ', process.env.NEXT_PUBLIC_API_URL_DEV);
+    console.log('Default API URL (PROD): ', process.env.NEXT_PUBLIC_API_URL_PRODUCT);
+    return [
+      {
+        source: process.env.SOURCE_PATH,
+        destination: dev ? process.env.NEXT_PUBLIC_API_URL_DEV : process.env.NEXT_PUBLIC_API_URL_PRODUCT,
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/kr/(.*)',
+        destination: '/',
+        permanent: true,
+      }
+    ]
+  }
 };
 
 export default withVanillaExtract(nextConfig);
---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+const getCookie = function (name: string) {
+    if (typeof window !== "undefined") {
+        const value = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
+        console.log('cookie value', value)
+        return value ? value[2] : null;
+    }
+};
+
+const prod = process.env.NODE_ENV === 'production';
+const axiosInstance = axios.create({
+    baseURL: prod ? process.env.NEXT_PUBLIC_API_URL_PRODUCT : process.env.NEXT_PUBLIC_API_URL_DEV,
+    timeout: 1000,
+    headers: {
+        'Content-Type': 'application/json',
+        authorization: null,
+    }
+});
+
+axiosInstance.interceptors.request.use(
+    function (config) {
+        const token = getCookie('LOGIN_COOKIE');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+)
+export default axiosInstance;
