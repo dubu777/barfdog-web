cf83568add6cbc18456b7e786270df552230d87d
jkeFreshour <jke@freshour.co.kr>
2025-01-13 10:30:20 +0900

    feat: 장바구니 상품 개수 증가/감소, 선택삭제, 개별 삭제 로직 개발 및 비즈니스 로직 분리
    1. 장바구니 mutation 적용
    2. 장바구니 상품 개수 증가/감소 로직 개발
    3. 장바구니 선택삭제, 개별 삭제 로직 개발
    4. 비즈니스 로직 store -> utils 분리 (calculateSummary, filterSelectedItems)

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { CartInfo, CartSummary } from "@/types/cart";
+import { calculateSummary } from "@/utils/cart/calculateSummary";
+import { filterBySelectedItems } from "@/utils/cart/filterBySelectedItems";
 
 interface CartStore {
   count: number;
@@ -20,41 +22,14 @@ const initialCalculatedPrices = {
   totalOrderPrice: 0,
 }
 
-const filterSelectedBasketDtoList = (cartInfo: CartInfo, selectedItems: number[]) =>
-  cartInfo.basketDtoList.filter(item => selectedItems.includes(item.itemDto.basketId));
-
 export const useCartStore = create<CartStore>((set, get) => ({
   count: 0,
   cartInfo: null,
   selectedItems: [],
   calculatedPrices: initialCalculatedPrices,
-  // 주문 요약 정보 계산
-  calculateSummary: (cartInfo) => {
-    // 할인 금액 (discount),
-    // 상품 총 금액 (productTotalPrice)
-    // 배송비 (deliveryFee)
-    // 최종 주문 금액 (totalOrderPrice)
-    // 무료 배송 조건까지 필요한 금액 (diffDeliveryFee)
-    const discount = cartInfo.basketDtoList.reduce(
-      (sum, item) => sum + (item.itemDto.originalPrice - item.itemDto.salePrice) * item.itemDto.amount,
-      0
-    );
-    const productTotalPrice = cartInfo.basketDtoList.reduce((sum, item) => sum + item.totalPrice, 0) + discount;
-    const deliveryFee =
-      productTotalPrice >= cartInfo.deliveryConstant.freeCondition
-        ? 0
-        : cartInfo.deliveryConstant.price;
-    const totalOrderPrice = productTotalPrice - discount + deliveryFee;
-    const diffDeliveryFee =
-      productTotalPrice >= cartInfo.deliveryConstant.freeCondition
-        ? 0
-        : cartInfo.deliveryConstant.freeCondition - totalOrderPrice;
 
-    return { productTotalPrice, discount, deliveryFee, totalOrderPrice, diffDeliveryFee };
-  },
   // 장바구니 데이터 초기화 및 전체 데이터를 업데이트 시 (전체선택 상태 기본값 설정 및 장바구니 항목 수, 선택된 항목 업데이트 및 요약 정보 계산)
   setCartInfo: (cartInfo) => {
-    const { calculateSummary } = get();
     if (!cartInfo) {
       console.error("Invalid cart data");
       return;
@@ -72,7 +47,7 @@ export const useCartStore = create<CartStore>((set, get) => ({
   },
   // 장바구니 개별 항목 수량 변경 시 (선택된 항목에 맞춘 데이터 필터링, 특정 항목 수량 업데이트 후 총 가격 계산 및 요약 정보 계산)
   updateItemAmount: (basketId, newAmount) => {
-    const { cartInfo, calculateSummary, selectedItems } = get();
+    const { cartInfo, selectedItems } = get();
     if (!cartInfo) {
       console.error("Cart data is not available");
       return;
@@ -99,7 +74,7 @@ export const useCartStore = create<CartStore>((set, get) => ({
       ? selectedItems
       : [...selectedItems, basketId];
 
-    const filteredBasketDtoList = filterSelectedBasketDtoList(updatedCartInfo, updatedSelectedItems);
+    const filteredBasketDtoList = filterBySelectedItems(updatedCartInfo, updatedSelectedItems);
     const filteredCartInfo = { ...updatedCartInfo, basketDtoList: filteredBasketDtoList };
 
     const calculatedPrices = calculateSummary(filteredCartInfo);
@@ -112,13 +87,13 @@ export const useCartStore = create<CartStore>((set, get) => ({
   },
   // 장바구니 선택한 항목 업데이트 시 (선택한 항목을 기반으로 ID Array 업데이트 및 요약 정보 다시 계산)
   setSelectedItems: (selectedItems) => {
-    const { cartInfo, calculateSummary } = get();
+    const { cartInfo } = get();
     if (!cartInfo) {
       console.error("Cart data is not available");
       return;
     }
 
-    const selectedBasketDtoList = filterSelectedBasketDtoList(cartInfo, selectedItems);
+    const selectedBasketDtoList = filterBySelectedItems(cartInfo, selectedItems);
     const filteredCartInfo = { ...cartInfo, basketDtoList: selectedBasketDtoList };
     const calculatedPrices = calculateSummary(filteredCartInfo);
 
