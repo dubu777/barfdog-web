a2a30ce48e9a9df03003162c5d2305c1c7584ee3
jkeFreshour <jke@freshour.co.kr>
2025-04-10 10:55:10 +0900

    feat: middleware login 리디렉션시 cookie 값 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getNeedToSetPassword } from "@/api/auth/auth";
 import { AUTH_CONFIG } from "@/constants/auth";
 import { isAuthenticated } from "./utils/auth/isAuthenticated";
+import { cookies } from "next/headers";
 
 
 const pathsRequiringPasswordSetup = [
@@ -37,20 +38,30 @@ export async function middleware(req: NextRequest) {
 	const pathname = new URL(req.url).pathname;
 	let needToSetPassword = false;
 
+	const logoutWithDeleteCookies = () => {
+		const response = NextResponse.redirect(new URL('/login', req.nextUrl.origin));
+
+		const cookiesToDelete = [AUTH_CONFIG.ACCESS_TOKEN_COOKIE, AUTH_CONFIG.REFRESH_TOKEN_COOKIE];
+		cookiesToDelete.forEach(cookie => {
+			response.cookies.set(cookie, '', { path: '/', maxAge: 0 })
+		})
+
+		return response;
+	}
+
 	try {
 		needToSetPassword = await shouldRedirectToSetPassword(pathname);
 	} catch (error) {
 		console.error('비밀번호 설정 여부 확인 중 오류 발생:', error);
 		// 에러 발생 시 로그인 페이지로 리디렉션
-		return NextResponse.redirect(new URL('/login', req.nextUrl.origin));
+		return logoutWithDeleteCookies();
 	}
 
-	console.log('needToSetPassword', needToSetPassword)
   // 보호된 경로에 대한 접근 체크
   if (isProtectedPath(pathname)) {
     const token = req.cookies.get(AUTH_CONFIG.ACCESS_TOKEN_COOKIE)?.value;
     if (!isAuthenticated(token)) {
-      return NextResponse.redirect(new URL('/login', req.nextUrl.origin));
+			return logoutWithDeleteCookies();
     }
   }
 
