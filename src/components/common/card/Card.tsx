17b254badf64bc2f80e89cc4477918639b595f15
jkeFreshour <jke@freshour.co.kr>
2025-06-19 11:39:17 +0900

    feat: 반려견 없을 경우 보여지는 empty UI animation 적용 및 컴포넌트 분리 (로그인 전, 로그인 후)

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -15,7 +15,7 @@ interface CardProps {
   className?: string;
   backgroundColor?: "none" | "gray0" | "gray50" | "gray100" | "transparent";
   borderRadius?: "none" | 8 | 12 | 16;
-  gap?: "none" | 4 | 8 | 12 | 16 | 20 | 32;
+  gap?: "none" | 4 | 8 | 12 | 16 | 20 | 32 | 40;
 }
 
 const Card = ({
