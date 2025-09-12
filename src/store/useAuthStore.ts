38460b616bd5b0e9878734034ac8ddaf44584912
jaehyeon <wjh5296@gmail.com>
2025-09-12 14:45:37 +0900

    feat: useAuthStore temp관련 로직 삭제

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,21 +1,14 @@
 import { create } from "zustand";
-import { TemporaryUserEmail, TemporaryPassword } from "@/types";
 import { LoginUserInfo, UserInfo } from "@/types";
-
 
 interface AuthStore {
   loginUserInfo: LoginUserInfo | null;
   setLoginUserInfo: (loginUserInfo: LoginUserInfo) => void;
   userInfo: UserInfo | null;
   setUserInfo: (userInfo: UserInfo) => void;
-  tempEmailUserInfo: TemporaryUserEmail;
-  setTempEmailUserInfo: (tempEmailUserInfo: TemporaryUserEmail) => void;
-  tempPwUserInfo: TemporaryPassword | null;
-  setTempPwUserInfo: (tempPwUserInfo: TemporaryPassword) => void;
   clientLoggedIn: boolean | null;
   setClientLoggedIn: (loggedIn: boolean | null) => void;
 }
-
 
 export const useAuthStore = create<AuthStore>((set) => ({
   userInfo: null,
@@ -30,17 +23,6 @@ export const useAuthStore = create<AuthStore>((set) => ({
       loginUserInfo: loginUserInfo,
     });
   },
-  tempEmailUserInfo: {
-    email: "",
-    provider: null,
-  },
-  setTempEmailUserInfo: (tempEmailUserInfo) => set({ tempEmailUserInfo }),
-  tempPwUserInfo: {
-    email: "",
-    name: "",
-    phoneNumber: "",
-  },
-  setTempPwUserInfo: (tempPwUserInfo) => set({ tempPwUserInfo }),
   clientLoggedIn: null,
   setClientLoggedIn: (loggedIn) => set({ clientLoggedIn: loggedIn }),
 }));
