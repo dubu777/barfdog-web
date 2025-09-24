8efca972a97b3cee39ce529978d488ddbc370860
jkeFreshour <jke@freshour.co.kr>
2025-09-24 16:21:48 +0900

    feat: 레시피(전성분 보기) v2 반영

--- src/api/recipes/recipes.ts
+++ src/api/recipes/recipes.ts
@@ -1,11 +1,14 @@
 import axiosInstance from "@/api/axiosInstance";
 import { AxiosInstance } from "axios";
-import { RecipeDto } from "@/types";
+import { ApiResponse, RecipeItem } from "@/types";
+import { validateApiResponse } from "@/utils/api/apiResponseUtils";
 
-const getRecipeList = async (instance: AxiosInstance = axiosInstance): Promise<RecipeDto[]> => {
-  const { data } = await instance.get('/api/recipes');
-  return data._embedded.recipeListResponseDtoList.sort((a, b) => a.id - b.id);
+const getRecipeList = async (instance: AxiosInstance = axiosInstance): Promise<RecipeItem[]> => {
+  const { data }: { data: ApiResponse<Record<string, RecipeItem[]>> } = await instance.get('/api/v2/recipes/public');
+  const responseData = validateApiResponse(data, '레시피 목록 조회에 실패했습니다.');
+  return responseData.recipeList;
 }
+
 export { 
   getRecipeList
 };
