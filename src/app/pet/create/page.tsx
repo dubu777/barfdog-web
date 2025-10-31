8e8547d954c98ffc8d14a8b9840cfb0d33748f10
jaehyeon <wjh5296@gmail.com>
2025-10-31 14:28:46 +0900

    feat: getPetBreedList prefetch 추가

--- src/app/pet/create/page.tsx
+++ src/app/pet/create/page.tsx
@@ -1,4 +1,13 @@
+import { prefetchGetPetBreedList } from "@/api/pet/queries/prefetchGetPetBreedList";
 import PetCreateForm from "@/components/pages/pet/create/PetCreateForm";
+import Spinner from "@/components/common/spinner/Spinner";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PetCreatePageProps {
   searchParams: {
@@ -6,7 +15,21 @@ interface PetCreatePageProps {
   };
 }
 
-export default function PetCreatePage({ searchParams }: PetCreatePageProps) {
+export default async function PetCreatePage({
+  searchParams,
+}: PetCreatePageProps) {
+  const queryClient = new QueryClient();
+  await prefetchGetPetBreedList(queryClient);
+  const dehydrateState = dehydrate(queryClient);
+
   const source = searchParams?.source ?? "diet-analysis";
-  return <PetCreateForm source={source} />;
+  return (
+    <HydrationBoundary state={dehydrateState}>
+      <ErrorBoundary fallback={<div>Something went wrong.</div>}>
+        <Suspense fallback={<Spinner fullscreen />}>
+          <PetCreateForm source={source} />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrationBoundary>
+  );
 }
