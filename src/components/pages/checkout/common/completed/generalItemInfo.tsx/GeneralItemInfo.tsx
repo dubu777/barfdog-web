9afe1156918479ed1e0e89d70040a88294feac7d
jaehyeon <wjh5296@gmail.com>
2025-12-01 13:39:30 +0900

    feat: 빌드 에러 해결

--- src/components/pages/checkout/common/completed/generalItemInfo.tsx/GeneralItemInfo.tsx
+++ src/components/pages/checkout/common/completed/generalItemInfo.tsx/GeneralItemInfo.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useMemo } from "react";
 import Divider from "@/components/ui/divider/Divider";
 
 import { GeneralItem } from "@/types";
-import { usePaymentStore } from "@/store/checkout/usePaymentStore";
 import { commonWrapper } from "@/styles/common.css";
 import GeneralOrderItemCard from "../../../general/generalOrderItemList/generalOrderItemCard/GeneralOrderItemCard";
 import Card from "@/components/ui/card/Card";
@@ -13,27 +12,14 @@ interface GeneralItemInfoProps {
 }
 
 export default function GeneralItemInfo({ itemList }: GeneralItemInfoProps) {
-  const setFinalPrice = usePaymentStore((state) => state.setFinalPrice);
-
-  const finalPrice = useMemo(() => {
-    return itemList.reduce(
-      (sum, item) => sum + item.discountedItemAndOptionPrice,
-      0
-    );
-  }, [itemList]);
-
-  useEffect(() => {
-    setFinalPrice(finalPrice);
-  }, [finalPrice, setFinalPrice]);
-
   return (
     <Card gap={12} padding={12} align="start">
       <Text type="headline2">주문 상품</Text>
       <Divider thickness={2} color="gray900" />
       <div className={commonWrapper({ direction: "col", gap: 16 })}>
         {itemList.map((item, index, array) => (
-          <React.Fragment key={item.itemId}>
-            <GeneralOrderItemCard orderItemData={item} />
+          <React.Fragment key={item.id}>
+            <GeneralOrderItemCard generalItem={item} />
             {index < array.length - 1 && <Divider thickness={1} />}
           </React.Fragment>
         ))}
--- src/components/pages/checkout/general/completed/GeneralOrderCompleted.tsx
+++ src/components/pages/checkout/general/completed/GeneralOrderCompleted.tsx
@@ -1,68 +1,68 @@
-// "use client";
+"use client";
 
-// import DeliveryInfo from "../../common/completed/deliveryInfo/DeliveryInfo";
-// import PaymentInfo from "../../common/completed/paymentInfo/PaymentInfo";
-// import { completedContainer } from "../../OrderSheetCommon.css";
-// import Text from "@/components/ui/text/Text";
-// import { commonWrapper, pointColor } from "@/styles/common.css";
-// import ButtonDocked from "@/components/ui/buttonDocked/ButtonDocked";
-// import { useRouter } from "next/navigation";
-// import { usePersistOrderStore } from "@/store/checkout/usePersistOrderStore";
-// import { useGetGeneralCheckout } from "@/api/checkout/queries/useGetGeneralCheckout";
-// import Spinner from "@/components/ui/spinner/Spinner";
-// import GeneralItemInfo from "../../common/completed/generalItemInfo.tsx/GeneralItemInfo";
+import DeliveryInfo from "../../common/completed/deliveryInfo/DeliveryInfo";
+import PaymentInfo from "../../common/completed/paymentInfo/PaymentInfo";
+import { completedContainer } from "../../OrderSheetCommon.css";
+import Text from "@/components/ui/text/Text";
+import { commonWrapper, pointColor } from "@/styles/common.css";
+import ButtonDocked from "@/components/ui/buttonDocked/ButtonDocked";
+import { useRouter } from "next/navigation";
+import { usePersistOrderStore } from "@/store/checkout/usePersistOrderStore";
+import { useGetGeneralCheckout } from "@/api/checkout/queries/useGetGeneralCheckout";
+import Spinner from "@/components/ui/spinner/Spinner";
+import GeneralItemInfo from "../../common/completed/generalItemInfo.tsx/GeneralItemInfo";
 
-// export default function GeneralOrderCompleted({}) {
-//   const { itemList, clearItemList } = usePersistOrderStore();
-//   const { data: generalOrderData, isPending } = useGetGeneralCheckout({
-//     itemList,
-//   });
-//   const router = useRouter();
-//   const deliveryDto = {
-//     recipientName: "Mock 데이터", // 수령자 이름
-//     phoneNumber: "12312341234", // 수령자 전화번호
-//     zipcode: "123", // 우편번호
-//     street: "리", // 도로명 주소
-//     detailAddress: "13호", // 상세 주소
-//     request: "하하", // 배송 요청사항
-//     id: 12,
-//     deliveryName: "집",
-//     isDefault: true,
-//   };
+export default function GeneralOrderCompleted({}) {
+  const { itemList, clearItemList } = usePersistOrderStore();
+  const { data: generalOrderData, isPending } = useGetGeneralCheckout({
+    itemList,
+  });
+  const router = useRouter();
+  const deliveryDto = {
+    recipientName: "Mock 데이터", // 수령자 이름
+    phoneNumber: "12312341234", // 수령자 전화번호
+    zipcode: "123", // 우편번호
+    street: "리", // 도로명 주소
+    detailAddress: "13호", // 상세 주소
+    request: "하하", // 배송 요청사항
+    id: 12,
+    deliveryName: "집",
+    isDefault: true,
+  };
 
-//   if (isPending || !generalOrderData) {
-//     return <Spinner fullscreen />;
-//   }
+  if (isPending || !generalOrderData) {
+    return <Spinner fullscreen />;
+  }
 
-//   const handleGoToDetail = () => {
-//     clearItemList();
-//   };
+  const handleGoToDetail = () => {
+    clearItemList();
+  };
 
-//   const handleGoToHome = () => {
-//     clearItemList();
-//     router.push("/");
-//   };
+  const handleGoToHome = () => {
+    clearItemList();
+    router.push("/");
+  };
 
-//   return (
-//     <div className={completedContainer}>
-//       <div className={commonWrapper({ direction: "col", gap: 12 })}>
-//         <Text type="title2">
-//           <span className={pointColor}>주문이 완료</span>되었어요
-//         </Text>
-//         <Text type="body3" color="gray600">
-//           주문번호 123123123
-//         </Text>
-//       </div>
-//       <DeliveryInfo deliveryDto={deliveryDto} />
-//       <PaymentInfo paymentPrice={13000} paymentMethod="NAVER_PAY" />
-//       <GeneralItemInfo itemList={generalOrderData.itemList} />
-//       <ButtonDocked
-//         type="dual-button"
-//         primaryButtonLabel="구독 상세보기"
-//         secondaryButtonLabel="홈으로"
-//         onPrimaryClick={handleGoToDetail}
-//         onSecondaryClick={handleGoToHome}
-//       />
-//     </div>
-//   );
-// }
+  return (
+    <div className={completedContainer}>
+      <div className={commonWrapper({ direction: "col", gap: 12 })}>
+        <Text type="title2">
+          <span className={pointColor}>주문이 완료</span>되었어요
+        </Text>
+        <Text type="body3" color="gray600">
+          주문번호 123123123
+        </Text>
+      </div>
+      <DeliveryInfo deliveryDto={deliveryDto} />
+      <PaymentInfo paymentPrice={13000} paymentMethod="NAVER_PAY" />
+      <GeneralItemInfo itemList={generalOrderData.itemList} />
+      <ButtonDocked
+        type="dual-button"
+        primaryButtonLabel="구독 상세보기"
+        secondaryButtonLabel="홈으로"
+        onPrimaryClick={handleGoToDetail}
+        onSecondaryClick={handleGoToHome}
+      />
+    </div>
+  );
+}
